2078f9965c822502c943463aae6facefc2fc1d5e
bagasjs <nakanoyoichi83@gmail.com>
2025-08-03 04:00:51 +0700

    Fixing the LICENSE notice in lexer source code

--- src/lexer.h
+++ src/lexer.h
@@ -3,10 +3,28 @@
  * https://github.com/tsoding/b/blob/main/src/lexer.rs
  *
  * Original author: Tsoding
- * Licensed under the MIT License
+ * Licensed under the MIT License (just like what https://github.com/tsoding/b use)
  * 
  * Copyright 2025 Alexey Kutepov <reximkut@gmail.com> and the Contributors
- * See LICENSE for the full license text.
+ * Copyright 2025 bagasjs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 #ifndef LEXER_H_
@@ -27,6 +45,8 @@ typedef struct {
     int line_number;
 } Loc;
 
+#define HERE() (Loc){ __FILE__, -1, __LINE__ }
+
 #define compiler_missingf(loc, ...) _compiler_missingf(__FILE__, __LINE__, loc, __VA_ARGS__)
 void _compiler_missingf(const char *file, int line, Loc loc, const char *fmt, ...);
 void compiler_diagf(Loc loc, const char *fmt, ...);
@@ -46,6 +66,7 @@ void compiler_diagf(Loc loc, const char *fmt, ...);
     X(SEMICOLON    , ";"  ) \
     X(COLON        , ":"  ) \
     X(COMMA        , ","  ) \
+    X(ARROW        , "->") \
     X(MINUSMINUS   , "--" ) \
     X(MINUSEQ      , "-=" ) \
     X(MINUS        , "-"  ) \
@@ -84,7 +105,8 @@ void compiler_diagf(Loc loc, const char *fmt, ...);
     X(SWITCH       , "switch"    ) \
     X(GOTO         , "goto"      ) \
     X(RETURN       , "return"    ) \
-    X(FUNCTION     , "function"  ) \
+    X(FN           , "fn"        ) \
+    X(LET          , "let"       ) \
 
 typedef enum {
     // Terminal
